e272a35b35ad2ca4f565144473544531096f1d9c
pt545 <pt545@exeter.ac.uk>
2026-03-13 11:58:55 +0000

    Tasks 1-3

---
+++ TestPostApp.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class TestPostApp{
+
+    public static void main(){
+        Post post1 = new Post("Alex Adam","Help with JavaE","Hi, could anyone help me I need to learn how to code in java!");
+
+        System.out.println(post1.toString());
+
+        
+
+    }
+}
